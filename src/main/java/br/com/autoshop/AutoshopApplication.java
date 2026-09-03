11dbb951486bba5570a5d86a31be5c56457ae079
leticiaMalipense <leticia.malipense@gmail.com>
2026-09-02 21:54:43 -0300

    Create project

---
+++ src/main/java/br/com/autoshop/AutoshopApplication.java
@@ -0,0 +1,13 @@
+package br.com.autoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoshopApplication.class, args);
+	}
+
+}
