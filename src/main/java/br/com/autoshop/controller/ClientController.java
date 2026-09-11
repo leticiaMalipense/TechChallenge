504634f5a6d5118ad4ec30d1f83611ad7e6e8f64
leticiaMalipense <leticia.malipense@gmail.com>
2026-09-10 23:30:13 -0300

    Creating client controller

---
+++ src/main/java/br/com/autoshop/controller/ClientController.java
@@ -0,0 +1,15 @@
+package br.com.autoshop.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/client")
+public class ClientController {
+
+    @GetMapping
+    public String greeting() {
+        return "teste";
+    }
+}
